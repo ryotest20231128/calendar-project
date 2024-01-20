fb5496c5c0118ab315aa1ffb818469c249ec7179
ryotest20231128 <ryo.githubtest@gmail.com>
2024-01-20 17:20:44 +0900

    feat: カレンダーのナビゲーションを押した際に、先月、次月、今月のカレンダーを表示するようにした

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -3,19 +3,22 @@ import { CalendarHeader } from "../organisms/CalendarHeader";
 import { CalendarBody } from "../organisms/CalendarBody";
 import { useCalendar } from "../../hooks/useCalendar";
 import { CalendarNav } from "../organisms/CalendarNav";
+import { useState } from "react";
 
 export const CalendarPage = () => {
-  const today = new Date();
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-  const { dateList } = useCalendar({ currentDate: today });
+  const { dateList } = useCalendar({ currentDate: currentDate });
 
   return (
     <>
-      <h1 className="font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
-      <CalendarNav />
+      <h1 className="font-bold text-3xl mb-5">{`${
+        getMonth(currentDate) + 1
+      }月`}</h1>
+      <CalendarNav setCurrentDate={setCurrentDate} />
       <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
         <CalendarHeader />
-        <CalendarBody currentDate={today} dateList={dateList} />
+        <CalendarBody currentDate={currentDate} dateList={dateList} />
       </table>
     </>
   );
